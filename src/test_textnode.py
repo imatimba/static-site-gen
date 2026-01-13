b34aafcdd2e6100ad617dbdfbe46b402a8a92cdb
Jonathan Timbaldi <7434335+imatimba@users.noreply.github.com>
2026-01-12 23:19:33 -0300

    Implement text_node_to_html_node function and add corresponding tests for TextNode types

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -27,6 +27,38 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.TEXT)
         node2 = TextNode("This is a text node", TextType.TEXT, None)
         self.assertEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold text")
+
+    def test_italic(self):
+        node = TextNode("This is italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is italic text")
+
+    def test_image(self):
+        node = TextNode("An image", TextType.IMAGE, "https://example.com/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props, {"src": "https://example.com/image.png", "alt": "An image"}
+        )
+
+    def test_invalid_type(self):
+        node = TextNode("This is invalid", "invalid_type")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
